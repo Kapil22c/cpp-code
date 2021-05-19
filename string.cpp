3a9dae07fbeb8a4f542ffcc4c3f39537e41bee82
Kapil bavisiya <kapilbavisiya555@gmail.com>
2021-05-19 22:24:44 +0530

    C++ launchpad

--- string.cpp
+++ string.cpp
@@ -1,5 +1,4 @@
 #include<bits/stdc++.h>
-#include<string>
 using namespace std;
 bool compare(string a, string b){
 	if(a.length() == b.length()){
@@ -49,13 +48,82 @@ int main(){
 	// for(int i=0; i<n; i++){
 	// 	cout<<s[i]<<endl;
 	// }
-	char s[100] ="Today, is a rainy day";
-	char *ptr = mystrtok(s, ' ');
-	cout<<ptr<<endl;
+	// char s[100] ="Today, is a rainy day";
+	// char *ptr = mystrtok(s, ' ');
+	// cout<<ptr<<endl;
 	
-	while(ptr != NULL){
-		ptr = mystrtok(NULL, ' ');
-		cout<<ptr<<endl;
-	}	
+	// while(ptr != NULL){
+	// 	ptr = mystrtok(NULL, ' ');
+	// 	cout<<ptr<<endl;
+	// }	
+	string a;
+	string b("Kapil");
+	string c = "Bavisiya";
+	string d(c);
+	string e = d;
+	char f[] = {'k','a','p','i','l','\0'};
+	string g(f);
+
+	cout<<a<<endl;
+	cout<<b<<endl;
+	cout<<c<<endl;
+	cout<<d<<endl;
+	cout<<e<<endl;
+	cout<<f<<endl;
+	cout<<g<<endl;
+
+	if(a.empty()){
+		cout<<"it's empty"<<endl;
+	}
+
+	a.append("Hii there");
+	cout<<a<<endl;
+	a+= " also how's you?";
+	cout<<a<<endl;
+
+	//remove or erase the content
+	cout<<a.length()<<endl;
+	a.clear();
+	cout<<a.length()<<endl;
+
+	//compare string
+	a = "Apple";
+	b = "Mango";
+	cout<<a.compare(b)<<endl;
+	cout<<b.compare(a)<<endl;
+	cout<<a.compare(a)<<endl;
+
+	if(a>b){
+		cout<<"Apple is longer"<<endl;
+	}else{
+		cout<<"Mango is longer"<<endl;
+	}
+
+	string s = "I want to have apple juice";
+	int idx = s.find("apple");
+	cout<<idx<<endl;
+
+	string word = "apple";
+	int len = word.length();
+	s.erase(idx, len+1);
+	cout<<s<<endl;
+
+	for(int i=0; i<a.length(); i++){
+		cout<<a[i];
+		if(i!= a.length()-1){
+			cout<<":";
+		}
+	}
+	cout<<endl;
+	for(auto it=b.begin(); it!=b.end(); it++){
+		cout<<(*it);
+		if(it!=b.end()-1){
+			cout<<",";
+		}
+	}
+	cout<<endl;
+	for(auto x:a){
+		cout<<x<<".";
+	}
 	return 0;
 }
